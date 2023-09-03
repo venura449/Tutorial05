4ca27a638f5e0e4b255328471b7b9ed959fae760
J.A Venura Jayaasingha <137485589+venura449@users.noreply.github.com>
2023-09-03 23:28:08 +0530

    Update MExcersise 03.c

--- Tutorial 05/MExcersise 03.c
+++ Tutorial 05/MExcersise 03.c
@@ -6,45 +6,43 @@ struct Mark {
 
 int main() {
     struct Mark st1, st2, st3; // Declare variables to store student marks
-    int stCount = 0, i=0, sum=0, highestSum = 0, highestSumIndex = -1;
+    int stCount = 0, sum = 0, highestSum = 0, highestSumIndex = -1;
 
-    while (stCount < 3) // You probably meant to use a condition like 'while (stCount < 3)'
+    while (stCount < 3) // Input marks for three students
     {
-        
-        i=0; //setting subject no for 1 in each time excution
-      
+        int i = 0; // setting subject no for 1 in each time execution
+
         // Input student marks for subject 1
         printf("\nEnter the mark of student %d for subject %d: ", stCount + 1, i + 1);
         scanf("%d", &st1.marks1);
-        sum = sum + st1.marks1;
+        sum = st1.marks1;
         i++;
 
         // Input student marks for subject 2
         printf("Enter the mark of student %d for subject %d: ", stCount + 1, i + 1);
         scanf("%d", &st1.marks2);
-        sum = sum + st1.marks2;
+        sum += st1.marks2;
         i++;
 
         // Input student marks for subject 3
         printf("Enter the mark of student %d for subject %d: ", stCount + 1, i + 1);
         scanf("%d", &st1.marks3);
-        sum = sum + st1.marks3;
+        sum += st1.marks3;
         i++;
 
         // Input student marks for subject 4
         printf("Enter the mark of student %d for subject %d: ", stCount + 1, i + 1);
         scanf("%d", &st1.marks4);
-        sum = sum + st1.marks4;
-        i++;
-      
-         printf("\nSum of marks for student %d: %d\n", stCount + 1, sum);
+        sum += st1.marks4;
+
+        printf("\nSum of marks for student %d: %d\n", stCount + 1, sum);
 
         // Update highestSum and highestSumIndex if the current sum is higher
         if (sum > highestSum) {
             highestSum = sum;
             highestSumIndex = stCount;
         }
-        stCount += 1; // Increment the student count (but the loop will never run with the current condition)
+        stCount += 1; // Increment the student count
     }
 
     if (highestSumIndex != -1) {
